ccef2b9b28c537c79c22b6b8800f2bb98fc31a03
FabiozZz <mr.fabiozzz@gmail.com>
2023-11-03 21:39:46 +0300

    основной функционал готов

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,84 +1,178 @@
-import React from 'react';
-
+import React, {useEffect, useState} from 'react';
+import flag from 'flag-svgrepo-com.svg';
 interface Cell {
     row: number;
     col: number;
+    point:number|null,
+    bomb:boolean,
+    isHide: boolean;
+    flag:boolean
 }
 
-function randomInteger(min: number, max: number): number {
+function randomInteger(min:any, max:any) {
     // получить случайное число от (min-0.5) до (max+0.5)
     let rand = min - 0.5 + Math.random() * (max - min + 1);
     return Math.round(rand);
 }
-
-function generateBombs(count: number) {
-    let bombArr: Cell[] = []
+function generateBombs(count:any) {
+    let bombArr = []
     for (let bombs = 1; bombs <= count; bombs++) {
-        let row = randomInteger(1, 9)
-        let col = randomInteger(1, 9)
+        let row = randomInteger(1, 8)
+        let col = randomInteger(1, 8)
         while (row === col) {
-            col = randomInteger(1, 9)
+            col = randomInteger(1, 8)
+            // row = randomInteger(1, 8)
         }
-        bombArr.push({row, col});
+        const find = bombArr.find(_ => _.col === col && _.row === row);
+        if (find){
+            bombs--;
+        }else{
+            bombArr.push({row, col});
+        }
     }
     return bombArr
 }
 
-function generateBoardCell(arrBombs?: Cell[]) {
-    let arr: Array<Cell & { bomb: boolean, point: number; }> = [];
-    for (let row = 1; row < 10; row++) {
-        for (let col = 1; col < 10; col++) {
-            arr.push({row, col, point: 0, bomb: false})
+
+function generateMap(cols:any,rows:any,bombs:any){
+    const arr:any = [];
+    for (let row=0;row<rows;row++){
+        const temp = []
+        for (let col=0;col<cols;col++){
+            // const arrBombs = bombs.find((bomb) => bomb.row === row && bomb.col === col);
+            temp.push({point:0, bomb: false, isHide: true,row,col,flag:false})
+        }
+        arr.push(temp)
+    }
+
+    bombs.forEach((bomb:any)=>{
+        const cell = arr[bomb.row][bomb.col];
+        cell.bomb = true
+        cell.point = null
+        const arrChecks = [
+            {...bomb, row: bomb.row - 1},
+            {...bomb, row: bomb.row + 1},
+            {...bomb, col: bomb.col + 1},
+            {...bomb, col: bomb.col - 1},
+            {row: bomb.row - 1, col: bomb.col - 1},
+            {row: bomb.row - 1, col: bomb.col + 1},
+            {row: bomb.row + 1, col: bomb.col - 1},
+            {row: bomb.row + 1, col: bomb.col + 1}
+        ]
+        do{
+            const item = arrChecks[arrChecks.length - 1];
+            if (arr[item.row] && arr[item.row][item.col]) {
+                arr[item.row][item.col].point += 1;
+            }
+            arrChecks.pop()
+        } while (arrChecks.length )
+
+    })
+
+    return arr.flat()
+}
+function App() {
+    const [state,setState] = useState<Cell[]>([])
+    const [gameOver,setGameOver] = useState(false)
+
+    useEffect(() => {
+        const bombs = generateBombs(15);
+        setState(generateMap(9,9,bombs))
+    }, []);
+
+    function onClick(cell:Omit<Cell,'isHide'|'bomb'|'point'|'flag'>,state:Cell[]) {
+        console.log('cllick')
+        const findIndex = state.findIndex(_ => _.row === cell.row && _.col === cell.col);
+        const copyState = [...state];
+        if (findIndex >= 0) {
+            const find = copyState[findIndex]
+            if (find.bomb) {
+                setGameOver(true)
+                setState(prevState => prevState.map(c=>({...c,isHide:false})))
+                return;
+            }
+            if (!find.isHide) return
+            find.isHide = false
+            copyState.splice(findIndex,1,find)
+            if (find.point) {
+                setState(copyState)
+                return;
+            }
+
+            const arrChecks = [
+                {...cell, row: cell.row - 1},
+                {...cell, row: cell.row + 1},
+                {...cell, col: cell.col + 1},
+                {...cell, col: cell.col - 1},
+                {row: cell.row - 1, col: cell.col - 1},
+                {row: cell.row - 1, col: cell.col + 1},
+                {row: cell.row + 1, col: cell.col - 1},
+                {row: cell.row + 1, col: cell.col + 1}
+            ];
+            do{
+                const item = arrChecks[arrChecks.length - 1];
+                if (item) {
+                    onClick(item,copyState);
+                }
+                arrChecks.pop()
+            }while (arrChecks.length)
         }
     }
-    arr.map(cell => {
-        arrBombs?.forEach(bomb => {
-            if (bomb.row === cell.row && bomb.col === cell.col) {
-                cell.bomb = true;
-            }
-        })
-        return cell
-    }).map(cell => {
-        arrBombs?.every(bomb => {
-            const prevRow: Cell = {...bomb, row: bomb.row - 1}
-            const nextRow: Cell = {...bomb, row: bomb.row + 1}
-            const nextCol: Cell = {...bomb, col: bomb.col + 1}
-            const prevCol: Cell = {...bomb, col: bomb.col - 1}
-            const prevTop: Cell = {row: bomb.row - 1, col: bomb.col - 1}
-            const prevBottom: Cell = {row: bomb.row - 1, col: bomb.col + 1}
-            const nextTop: Cell = {row: bomb.row + 1, col: bomb.col - 1}
-            const nextBottom: Cell = {row: bomb.row + 1, col: bomb.col + 1}
-            if (
-                (cell.row === prevRow.row && cell.col === prevRow.col) ||
-                (cell.row === nextRow.row && cell.col === nextRow.col) ||
-                (cell.row === nextCol.row && cell.col === nextCol.col) ||
-                (cell.row === prevCol.row && cell.col === prevCol.col) ||
-                (cell.row === prevTop.row && cell.col === prevTop.col) ||
-                (cell.row === prevBottom.row && cell.col === prevBottom.col) ||
-                (cell.row === nextTop.row && cell.col === nextTop.col) ||
-                (cell.row === nextBottom.row && cell.col === nextBottom.col)
-            ) {
-              cell.point = cell.point + 1;
-            }
-            return true
-        });
-        return cell
-    })
-    return arr
-}
+    function onContext(cell:Omit<Cell,'isHide'|'bomb'|'point'|'flag'>) {
+        const findIndex = state.findIndex(_ => _.row === cell.row && _.col === cell.col);
+        const copyState = [...state];
+        if (findIndex >= 0) {
+            const find = copyState[findIndex]
+            find.flag = !find.flag
+            copyState.splice(findIndex,1,find)
+            setState(copyState)
+        }
+    }
 
-function App() {
-    const bombs = generateBombs(10);
-    const generate = generateBoardCell(bombs)
+    const renderCell = (cell:Cell) => {
+        if (cell.flag) return <img src={flag} alt="flag"/>
+        if (cell.isHide) return null;
+        if (!cell.bomb) return cell.point || null
+        return "B"
+    };
+
+    const getCellStyle = (cell:Cell) => {
+        if (cell.isHide || cell.flag) return 'darkgray'
+        if (cell.point === 0) return 'gray'
+        if (cell.bomb) return 'red'
+        return 'darkgray'
+    };
 
     return (
+        <>
         <div className={'board'}>
-            {generate.map(_ => {
-                return <div className={'cell'} style={{background:_.bomb?'red':'gray'}}>{_.bomb?null:_.point?_.point:null}</div>
+            {state.map((_,index) => {
+                return <div key={index}
+                            onClick={(e)=> {
+                                e.stopPropagation()
+                                if (gameOver || _.flag) return
+                                const {row,col} = _
+                                onClick({row,col},state)
+                            }}
+                            onContextMenu={(e)=>{
+                                e.preventDefault()
+                                const {row,col} = _
+                                onContext({row,col})
+                            }}
+                            className={'cell'}
+                            style={{
+                                background:getCellStyle(_)
+                }}>{renderCell(_)}</div>
             })}
 
         </div>
+            <button onClick={()=> {
+                const bombs = generateBombs(15);
+                setState(generateMap(9,9,bombs))
+                setGameOver(false)
+            }}>new game</button>
+        </>
     );
 }
-
+//
 export default App;
